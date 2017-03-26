981c38c7acda90683e7bc5f4dabf391e166cdefe
charles-r-earp <charles.r.earp@gmail.com>
2017-03-25 23:01:55 -0700

    motor control

--- examples/test_motor.cpp
+++ examples/test_motor.cpp
@@ -4,7 +4,7 @@ int main() {
     
     i2c_linux::pwm_driver pwm_driver; // defaults to adress of 0x40 and 60 hz
     
-    i2c_linux::pwm_driver::bi_directional_motor motor(4, 5); // Channels 4 and 5
+    //i2c_linux::pwm_driver::bi_directional_motor motor(4, 5); // Channels 4 and 5
     
     while (true) {
         
